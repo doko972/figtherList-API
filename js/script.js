fb4012314692f064a812cb4f772e0ef88a411700
Doko <44224863+doko972@users.noreply.github.com>
2024-04-23 11:23:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-const ul = document.getElementById('figtherList');
 
-async function getListHero() {
-    try {
-        const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json');
-        const figtherList = await response.json();
+let selectedCharacters = [];
 
-        figtherList.forEach(hero => {
-            const name = document.createElement('li');
-            name.innerText = hero.name;
-            ul.appendChild(name);
+fetch('https://akabab.github.io/superhero-api/api/all.json')
+    .then(response => response.json())
+    .then(data => {
+        const superheroList = document.getElementById('superheroList');
+
+        data.forEach(hero => {
+            const superheroDiv = document.createElement('div');
+            superheroDiv.classList.add('superhero');
+
+            superheroDiv.innerHTML = `
+                <img src="${hero.images.sm}" alt="${hero.name}">
+                <button id=" ${hero.id}">Select:${hero.name}</button>
+            `;
+            superheroList.appendChild(superheroDiv);
+
+            
+            const heroButton = superheroDiv.querySelector('button');
+            heroButton.addEventListener('click', function () {
+                console.log('Le bouton', hero.name, 'a été cliqué !');
+            });
         });
-
-        return true;
-    }
-    catch (e) {
-        console.error('problème', e);
-        return false;
-    }
-}
-
-// const o = getListHero();
-
-function displaySuperheroes(superheroes){
-    const superheroesList = document.getElementById('superheroes-list');
-    superheroes.forEach(superhero => {
-
+    })
+    .catch(error => {
+        console.error('Error fetching superheroes:', error);
     });
-}
+
+
+
+
 
 
 
